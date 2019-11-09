1cb891e1cd0611513fec2020f304929b93dd53a8
Janis Knets <janis.knets@accenture.com>
2019-11-09 10:39:05 +0200

    Izveidoju pamata piemerus
    pieliku ceļus
       /bildes
       /bilde/bildesID
       /komentari/komentaraID
    Pamata apraksts kā strādat ar template'iem
    Pieliku piemēru kā lasīt no teksta faila

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+  vards = request.args.get('vards', default = 'pasaule', type = str)
+  return render_template('sveikaPasaule.html', vards = vards)
+
+@app.route('/bildes')
+def visasBildes():
+  return render_template('home.html')
+
+@app.route('/bilde/<bildesID>')
+def bilde(bildesID):
+  return render_template('bilde.html', bildesID = bildesID)
+
+@app.route('/komentari/<komentaraID>')
+def komentars(komentaraID):
+  atrastaisKomentars = ""
+  try:
+    fp = open('komentari.txt')
+    line = fp.readline()
+    cnt = 0
+    while line:
+      print("cnt: {}, komentaraID: {}".format(cnt, komentaraID))
+      if cnt == int(komentaraID):
+        print("Line {}: {}".format(cnt, line.strip()))
+        atrastaisKomentars = line
+      line = fp.readline()
+      cnt += 1
+  finally:
+    fp.close()
+  return render_template('komentars.html', komentars = atrastaisKomentars)
+
+@app.route('/health')
+def health():
+  return "OK"
+
+if __name__ == '__main__':
+  app.run()
